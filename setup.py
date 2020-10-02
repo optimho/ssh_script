7e67e8538e0bbf9819776e079ffb44a7b3e289a8
optimho <michael@duplessis.nz>
2020-10-02 21:47:02 +1300

    Update README.md
    FireControl gives an easy way for one to enable firewall rules.
    Firewall rules that have been pre-configured can be turned off in the edge router.
    run the script from a python enabled device on the local network and quickly block traffic to an the internet from a specific mac or ip address.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fire Control',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='michael',
+    author_email='michael@duplessis.nz',
+    description='Run scripts too enable and disable firewall rules'
+)
