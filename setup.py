4493c39670345f8a85b0b7e48697975b2d7e568f
optimho <michael@duplessis.nz>
2020-10-03 23:03:42 +1300

    Updated package

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="firecontrol", # Replace with your own username
     version="0.0.1",
